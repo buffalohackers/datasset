13fda66dd8839ec3f5195e5b877893aaacd6009b
Nick DiRienzo <nick@dirienzo.co>
2013-10-05 19:42:37 -0400

    Swaps out images. Wow, that took way too long.

--- datass.js
+++ datass.js
@@ -1,1 +1,4 @@
-console.log("Datass.");
+chrome.webRequest.onBeforeRequest.addListener(function(r) {
+    p.console.log(r.url);
+    return {redirectUrl: "chrome://blank"}
+}, {"urls": ["http://*/*", "https://*/*"], "types": ["image"]}, ["blocking"]);
