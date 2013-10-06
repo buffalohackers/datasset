32d7abf259d4d0f8015e3687df5d9b03cf6f7146
Nick DiRienzo <nick@dirienzo.co>
2013-10-06 05:52:55 -0400

    Remote fetching and caching. Can replace an image on a request with bytes in the local storage cache. Works locally.

--- datass2.js
+++ datass2.js
@@ -1,29 +1,60 @@
+var cached = true;
+var keys = [];
+chrome.storage.local.get(null, function(items) {
+    keys = Object.keys(items);
+});
+
+var peerKeys = {}; // {key: [peer, peer...], ...}
+//connection webrtc
+//load other keys
+
 chrome.webRequest.onBeforeRequest.addListener(function(r) {
     var url = r.url;
-    console.log("Request: " + url);
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log("Sending a message to: " + tabs[0].id);
-        chrome.tabs.sendMessage(tabs[0].id, {imgSlug: "thing", type: "request"}, function(resp) {
-            console.log(resp.cached);
-            if (!resp.cached) {
-                console.log("Not cached. Attempting to get from peer.");
-                return;
-            }
-            if(resp.failed) {
-                console.log("Not in cache and not near peers. Hitting server.");
-                return;
-            }
-            console.log("Returning blank image.");
-            return {redirectUrl: "chrome://blank"};
+    var inCache = false;
+    for(var i = 0; i < keys.length; i++) {
+        if(keys[i] === url) {
+            inCache = true;
+            break;
+        }
+        console.log(i+1 + "/" + keys.length + ": " + keys[i]);
+    }
+    console.log(inCache);
+    console.log("Request: " + url + "\t" + keys);
+    if (inCache) {
+        console.log("Local cache hit.");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log("sending message to: " + tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, {key: url, type: "local"});
         });
-    });
+        return {redirectUrl: "chrome://blank"};
+    } else if (url in peerKeys) { //if in someone elses peer
+        //send a request for it to the peer
+        console.log("Peer cache hit.");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log("sending message to: " + tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, {key: url, type: "local"}); // send other peer info
+        });
+        return {redirectUrl: "chrome://blank"};
+    } else {
+        console.log("Hitting remote server.");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            console.log("sending message to: " + tabs[0].id);
+            chrome.tabs.sendMessage(tabs[0].id, {key: url, type: "remote"});
+        });
+        return;
+    }
 }, {"urls": ["http://*/*", "https://*/*"], "types": ["image"], "tabId": null}, ["blocking"]);
 
-chrome.webRequest.onCompleted.addListener(function(r) {
-    var url = r.url;
-    console.log("Response: " + url);
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        console.log("Sending a resp message to: " + tabs[0].id);
-        chrome.tabs.sendMessage(tabs[0].id, {imgSlug: "thing", type: "response"});
-    });
-}, {"urls": ["http://*/*", "https://*/*"], "types": ["image"], "tabId": null});
+chrome.runtime.onMessage.addListener(
+    function(msg, sender, sendResponse) {
+        if(msg.type === "update") {
+            console.log("Updating local cache keys.");
+            console.log(msg);
+            keys.push(msg.key);
+            console.log(keys);
+        }
+});
+
+//wait for callback from peer
+//load it into local storage
+//tell the front end its in local storage
--- datass_fg.js
+++ datass_fg.js
@@ -1,74 +1,55 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    console.log(msg.imgSlug);
-    var slug = msg.imgSlug;
-    if(msg.type === "request") {
-        chrome.storage.local.get("thing", function(r) {
-            console.log("Stoarge Get: " + JSON.stringify(r));
-            if($.isEmptyObject(r)) {
-                console.log("Empty object in cache.");
-                sendResponse({cached: true});
-                return true;
-            }
+chrome.runtime.onMessage.addListener(function(msg, sender, sendMessage) {
+    // Local cache image
+    if(msg.type === "local") {
+        chrome.storage.local.get(msg.key, function(item) {
+            console.log(item);
+            console.log(item[msg.key].data);
+            // Swap the img src with bytes
+            var i = $("img[src$='" + item[msg.key].filename + "']").attr("src", item[msg.key].data);
+            console.log(i);
         });
-    } else if(msg.type === "response") {
-        console.log("Saving " + slug + " to local storage.");
-        chrome.storage.local.set({slug: "thing"});
+    } else if(msg.type === "remote") {
+        // Image was fetched from a remote server 
+        var store = {},
+            url = msg.key,
+            urlParts = url.split("/"),
+            filename = urlParts[urlParts.length - 1],
+            
+            img = new Image(),
+            imgWidth = 0,
+            imgHeight = 0,
+
+            imgCanvas = document.createElement("canvas"),
+            imgContext = imgCanvas.getContext("2d");
+
+            img.onload = function() {
+                // YOLO
+                imgWidth = this.width;
+                imgHeight = this.height;
+                console.log(imgWidth);
+                console.log(imgHeight);
+                console.log("created canvas");
+                $(imgCanvas).addClass("datass");
+                imgCanvas.width = imgWidth;
+                imgCanvas.height = imgHeight;
+                console.log("about to draw");
+                imgContext.drawImage(this, 0, 0, imgWidth, imgHeight);
+                console.log("drawn");
+                // Convert to base64
+                var data = imgCanvas.toDataURL("image/png");
+                console.log("data: " + data);
+                var raw = {};
+                store["key"] = url;
+                store["data"] = data;
+                store["filename"] = filename;
+                raw[url] = store;
+                console.log("Storing: " + JSON.stringify(store));
+                // Store in local storage
+                chrome.storage.local.set(raw);
+                store["type"] = "update";
+                // Send message to update backend keys
+                chrome.runtime.sendMessage(store);
+            };
+            img.src = url;
     }
-    return true;
-    //sendResponse({exists: true});
 });
-
-/*var port = chrome.runtime.connect({name: "datass"});
-
-port.onMessage.addListener(function(msg) {
-    console.log(msg);
-    var slug = msg.imgSlug;
-    if(slug != "") {
-        console.log(slug);
-        chrome.storage.local.set({slug: "test"}, function() {
-            port.postMessage({added: slug});   
-        });
-        chrome.storage.local.get(slug, function(q) {
-            console.log("get");
-            console.log(q);
-        });
-    }
-});*/
-
-/*console.log("hey");
-chrome.storage.local.set({"test": "value"});
-chrome.storage.local.get("test", function(res) {
-    console.log(res);
-});
-chrome.runtime.onMessage.addListener(function(req, sender, cb) {
-    console.log(req);
-    console.log(req.imgUrl);
-    var url = req.imgUrl,
-        img = $("img[src$='" + url + "']");
-        imgCanvas = document.createElement("canvas"),
-        imgContext = imgCanvas.getContext("2d");
-    cb("image length: " + img.length);
-    if(img.length != 0) {
-        for(var i = 0; i < img.length; i++) {
-            console.log(img);
-            console.log(img.width);
-            console.log(img.height);
-            console.log("created canvas");
-            $(imgCanvas).addClass("datass");
-            $(".datass").hide();
-            imgCanvas.width = img.width;
-            imgCanvas.height = img.height;
-            console.log("about to draw");
-            imgContext.drawImage(img, 0, 0, img.width, img.height);
-            console.log("drawn");
-            var data = imgCanvas.toDataURL("image/png");
-            console.log("data: " + data);
-            chrome.storage.local.set({url: data}, function() {
-                console.log("Saved image: " + url + " :: " + data);
-            });
-            $(".datass").remove();
-        }
-    } else {
-        alert("WHAT THE HELL");
-    }
-});*/
